49bb588d24f4aa18219f56f452cb815d58dac90c
tnorman <tnorma.dao@gmail.com>
2016-02-20 13:41:06 +0200

    add latlng to math

--- dist/tnr-math.js
+++ dist/tnr-math.js
@@ -15,7 +15,8 @@ var tnr = tnr || {};
     utility.positionLerp = function( c, m, p1, p2 ){ var _l = p2 - p1, s = _l / m, p = (s * c) + p1; return p;};
     utility.lerpV2 = function( num, x1, y1, x2, y2  ){ var R=[]; for( var _i = 0; _i < num; _i++){ R.push([ utility.toFix( utility.positionLerp( _i, num-1, x1, x2 ), 5 ), utility.toFix( utility.positionLerp( _i, num-1, y1, y2), 5 ) ]); }; return R; };
     utility.lerpV3 = function( num, pos1, pos2 ){ var R = []; for( var _i = 0; _i < num; _i++){ R.push([ utility.toFix( utility.positionLerp( _i, num-1, pos1.x, pos2.x ), 5 ), utility.toFix( utility.positionLerp( _i, num-1, pos1.y, pos2.y ), 5 ), utility.toFix( utility.positionLerp( _i, num-1, pos1.z, pos2.z ), 5 ) ]); }; return R; };
-
+    utility.xyz_latlng = function(P){ return { lt: Math.atan2(P.z, Math.sqrt(P.x * P.x + P.y * P.y)), ln: Math.atan2(P.y, P.x) };};
+    utility.latlng_xyz = function(lt,ln,R){ return { x:R*(Math.cos(lt)*Math.cos(ln)), y:R*(Math.cos(lt)*Math.sin(ln)), z:R*Math.sin(lt) } };
 
     utility.checkRadRange = function ( slave, master, R) {
       var _S = utility.radNormal( slave );
