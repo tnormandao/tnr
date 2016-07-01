b923db642f9a0fb79b0d7326596117077f34b60a
Konstantin Plaksiviy <tnorman.dao@gmail.com>
2016-07-01 11:20:50 +0300

    Update tnr-vec2.js

--- dist/tnr-vec2.js
+++ dist/tnr-vec2.js
@@ -1,6 +1,6 @@
 
 var Vec2 = function( x, y ){ this.constructor = Vec2; this.x = x || 0, this.y = y || 0; };
-Vec2.prototype.clone = function(){ var V = new this.constructor; V.x = this.x; V.y = this.y };
+Vec2.prototype.clone = function(){ var V = new this.constructor; V.x = this.x; V.y = this.y; return V; };
 Vec2.prototype.set = function( x, y ){ this.x = x || this.x; this.y = y || this.y; };
 Vec2.prototype.copy = function( other ){ this.x = other.x || this.x; this.y = other.y || this.y; }
 Vec2.prototype.plus = function( other ){ this.x += other.x; this.y += other.y; return this; };
@@ -12,3 +12,4 @@ Vec2.prototype.distance = function( other ){ return Math.sqrt( Math.abs(Math.pow
 Vec2.prototype.angleTo = function( other ){return((-Math.PI/2+ -(Math.atan2((other.y-this.y),(other.x-this.x))))+(Math.PI*2))%(Math.PI*2) };
 Vec2.prototype.pointOnRadius = function( A, R ){ var self = this; return { x: R*Math.sin(A) + self.x, y: R*Math.cos(A)+ self.y }; };
 Vec2.prototype.toAlphaByDimension = function(other){ return new this.constructor( this.x / other.x, this.y / other.y  ); };
+Vec2.prototype.sumxy = function(){ return Math.abs(this.x) + Math.abs(this.y) };
